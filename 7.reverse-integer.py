226acf8a1ff7a2ba49a14362b8d33f01a95e73e9
liubo <liubolovelife@gmail.com>
2019-08-26 16:23:26 +0800

    系统学习下

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -5,5 +5,8 @@
 #
 class Solution:
     def reverse(self, x: int) -> int:
-        
+        y = 0
+        if x > 2**32 - 1 or x < -2**32:
+            return 0
+#            y = y + () * 10**(i - 1)
 
