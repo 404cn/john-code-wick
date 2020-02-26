4ad61b6358e6a544e5644dc2236524d13b63b7c6
liubo <liubolovelife@gmail.com>
2020-02-26 23:12:01 +0800

    get to work

---
+++ golang/data-structures/linked-list/linked_list_test.go
@@ -0,0 +1,19 @@
+package list
+
+import (
+	"testing"
+)
+
+func TestNewList(t *testing.T) {
+	list := NewList()
+	if list.Length != 0 || list.Head != nil {
+		t.Errorf("List's length: %v, node: %v\n", list.Length, list.Head)
+	}
+}
+
+func TestNewNode(t *testing.T) {
+	node := NewNode("test")
+	if node.Value != "test" || node.Next != nil {
+		t.Errorf("Node's value: %v, next: %v\n", node.Value, node.Next)
+	}
+}
