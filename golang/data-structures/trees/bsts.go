b4a2efb2bbd040392e60f25a60cd4c3691edcccd
liubo <liubolovelife@gmail.com>
2020-03-05 23:49:05 +0800

    bsts

--- golang/data-structures/trees/bsts.go
+++ golang/data-structures/trees/bsts.go
@@ -23,7 +23,7 @@ type BSTs struct {
 	// bst's size
 	Size int
 	// bst's rooted node
-	Head *Node
+	Root *Node
 }
 
 // NewBst ...
@@ -32,41 +32,27 @@ func NewBst(n *Node) *BSTs {
 	if n == nil {
 		return &BSTs{}
 	}
-	return &BSTs{Head: n, Size: 1}
+	return &BSTs{Root: n, Size: 1}
 }
 
 // Insert ...
 // 插值
 func (bst *BSTs) Insert(value int) {
-	node := NewNode(value)
-	if bst.GetNodeCount() == 0 {
-		bst.Head = node
-		bst.Size++
+	insertNode(bst.Root, value)
+	bst.Size++
+}
+
+func insertNode(node *Node, value int) {
+	if node == nil {
+		node.Value = value
 		return
 	}
 
-	ptr := bst.Head
-	for {
-		if value < ptr.Value {
-			if ptr.Left == nil {
-				ptr.Left = node
-				node.Parent = ptr
-				break
-			} else {
-				ptr = ptr.Left
-			}
-		} else {
-			if ptr.Right == nil {
-				ptr.Right = node
-				node.Parent = ptr
-				break
-			} else {
-				ptr = ptr.Right
-			}
-		}
+	if value < node.Value {
+		insertNode(node.Left, value)
+	} else {
+		insertNode(node.Right, value)
 	}
-
-	bst.Size++
 }
 
 // GetNodeCount ...
@@ -78,37 +64,128 @@ func (bst *BSTs) GetNodeCount() int {
 // PrintValues ...
 // 从小到大打印树中的节点
 func (bst *BSTs) PrintValues() {
-	fmt.Printf("%v ", bst.Head.Value)
+	printValues(bst.Root)
 }
 
-// DeleteTree ...
-// 删除树
-func (bst *BSTs) DeleteTree()
+func printValues(node *Node) {
+	if node != nil {
+		printValues(node.Left)
+		fmt.Printf("%v ", node.Value)
+		printValues(node.Right)
+	}
+}
 
 // IsInTree ...
 // 如果值存在于树中则返回true
-func (bst *BSTs) IsInTree()
+func (bst *BSTs) IsInTree(value int) bool {
+	return isInTree(bst.Root, value)
+}
 
-// GetHeight ...
-// 返回节点所在的高度(如果只有一个节点, 那么高度则为1)
-func (bst *BSTs) GetHeight()
+func isInTree(node *Node, value int) bool {
+	if node == nil {
+		return false
+	}
+	if node.Value == value {
+		return true
+	} else {
+		return map[bool]bool{true: isInTree(node.Left, value), false: isInTree(node.Right, value)}[value < node.Value]
+	}
+}
+
+// GetHeight 返回节点所在的高度(如果只有一个节点, 那么高度则为1)
+func GetHeight(node *Node) int {
+	if node.Left == nil && node.Right == nil {
+		return 1
+	}
+
+	l := GetHeight(node.Left) + 1
+	r := GetHeight(node.Right) + 1
+
+	return map[bool]int{true: l, false: r}[l >= r]
+}
 
 // GetMin ...
 // 返回树上的最小值
-func (bst *BSTs) GetMin()
+func GetMin(node *Node) *Node {
+	curr := node
+
+	for curr.Left != nil {
+		curr = curr.Left
+	}
+
+	return curr
+}
 
 // GetMax ...
 // 返回树上的最大值
-func (bst *BSTs) GetMax()
+func GetMax(node *Node) *Node {
+	curr := node
+
+	for curr.Right != nil {
+		curr = curr.Right
+	}
+
+	return curr
+}
 
 // IsBinarySearchTree ...
 // 是否为binnary search tree
-func (bst *BSTs) IsBinarySearchTree()
+func IsBinarySearchTree(root *Node) bool {
+	if root.Left == nil && root.Right == nil {
+		return true
+	}
+
+	if root.Left == nil || root.Right == nil {
+		return false
+	}
+
+	if root.Left.Value >= root.Value || root.Right.Value < root.Value {
+		return false
+	}
+
+	return IsBinarySearchTree(root.Left) && IsBinarySearchTree(root.Right)
+}
 
 // DeleteValue ...
 // 根据值删除树上的节点
-func (bst *BSTs) DeleteValue()
+func DeleteValue(value int, root *Node) *Node {
+	// TODO parent node
+	if root == nil {
+		return nil
+	}
+
+	if value < root.Value {
+		root.Left = DeleteValue(value, root.Left)
+	} else if value > root.Value {
+		root.Right = DeleteValue(value, root.Right)
+	} else {
+		if root.Left == nil {
+			root = root.Right
+		} else if root.Right == nil {
+			root = root.Left
+		} else {
+			// 用后继节点替换要删除的节点
+			// 因为后继节点是比要删除的节点的值大的最小的节点
+			d := GetSuccessor(root)
+			root.Value = d.Value
+			root.Right = DeleteValue(d.Value, root.Right)
+		}
+	}
+
+	return root
+}
 
 // GetSuccessor ...
 // 返回给定值的后继
-func (bst *BSTs) GetSuccessor()
+func GetSuccessor(node *Node) *Node {
+	if node.Right != nil {
+		return GetMin(node.Right)
+	}
+
+	y := node.Parent
+	for y != nil && node == y.Right {
+		node, y = y, y.Parent
+	}
+
+	return y
+}
